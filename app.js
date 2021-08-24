9bdaee6c59d31f5623cf28713fe54f211747a819
CP Tan <spin555@hotmail.com>
2021-08-24 17:24:40 +0800

    add error handler

--- app.js
+++ app.js
@@ -8,6 +8,50 @@ app.get("/", (req, res) => {
   res.send("Welcome to my homepage");
 });
 
+// this function simulates one asynchronous operation,
+// e.g. loading user profile from database after 10ms
+const newSong = {
+  name: "newSongName",
+  artist: "newSongArtist",
+};
+
+const loadSongsDB = (songID) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(newSong), 3000);
+  });
+};
+
+const loadSongDBFailed = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error("Network Error")), 10);
+  });
+};
+
+// the async function using async/await
+const getSongById = async (req, res, next) => {
+  const songID = req.params.id;
+
+  try {
+    const song = await loadSongsDB(songID);
+    res.send(song);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// register the asynchronous handler function to a path
+app.get("/mockDB/songs/:id", getSongById);
+
+app.get("/mockDB/fail", async (req, res, next) => {
+  try {
+    const result = await loadSongDBFailed();
+    res.send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//default error handling
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   res.status(err.statusCode).send(err.message);
--- app.test.js
+++ app.test.js
@@ -13,3 +13,21 @@ describe("App", () => {
     expect(response.text).toEqual("Welcome to my homepage");
   });
 });
+
+describe("Name of the group", () => {
+  it("GET /mockDB/songs/:id should return the correct song", async () => {
+    const expectedSong = { name: "newSongName", artist: "newSongArtist" };
+
+    const { body: actualSong } = await request(app)
+      .get("/mockDB/songs/1")
+      .expect(200);
+
+    expect(actualSong).toMatchObject(expectedSong);
+  });
+});
+
+describe("Get /mockDB/fail", () => {
+  it("Should return 500 error when server is down", async () => {
+    const response = await request(app).get("/mockDB/fail").expect(500);
+  });
+});
